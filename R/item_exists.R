d28a05b5a865088e81fc56c2a75ae418e3b06bce
Jordan S Read <jread@usgs.gov>
2014-12-11 11:02:56 -0600

    adding item_exists()

---
+++ R/item_exists.R
@@ -0,0 +1,23 @@
+#'@title check if identifier touple already exists on SB
+#'@description returns TRUE if touple already belongs to a sciencebase item, FALSE if not
+#'
+#'@param scheme
+#'@param type
+#'@param key
+#'@param session
+#'@return boolean for whether item exists
+#'
+#'@examples
+#'\dontrun{
+#'item_exists('mda_streams','ts_doobs','nwis_01018035')
+#'item_exists('mda_streams','site_root','nwis_01018035')
+#'}
+#'@export
+item_exists = function(scheme, type, key, session = NULL){
+	items <- query_item_identifier(scheme, type, key, session)
+	if (nrow(items) > 0){
+		return(TRUE)
+	} else {
+		return(FALSE)
+	}
+}
