f210d1f37f9b928a135294ad8c6fa3156e3e007b
Luke Winslow <lwinslow@usgs.gov>
2014-12-12 14:29:33 -0600

    Fleshed out item_file_download

--- R/item_file_download.R
+++ R/item_file_download.R
@@ -5,6 +5,24 @@
 #'
 #'
 #'@export
-item_file_download = function(id, names, destination){
+item_file_download = function(id, names, destinations, session=NULL){
 	
+	if(length(names) != length(destinations)){
+		stop('Length of names and destinations must be identical')
+	}
+	
+	flist = item_list_files(id, session)
+	
+	
+	if(!all(names %in% flist$fname)){
+		stop('Item does not contain all requested files')
+	}
+	
+	flist = merge(flist, data.frame(fname=names, dest=destinations, stringsAsFactors=FALSE))
+	
+	for(i in 1:nrow(flist)){
+		GET(url=flist[i,]$url, handle=session, write_disk(flist[i,]$dest))
+	}
+	
+	return(TRUE)
 }
