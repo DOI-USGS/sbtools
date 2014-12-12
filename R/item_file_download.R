3f46abd69ab8c5d8cf4e385510ff64debc5285ac
Luke Winslow <lwinslow@usgs.gov>
2014-12-12 14:55:42 -0600

    Fixed item_file_download documentation.

--- R/item_file_download.R
+++ R/item_file_download.R
@@ -1,8 +1,12 @@
 #'
 #'@title Download files attached to item
 #'
+#'@param id SB item ID
+#'@param names String vector list of file names attached to item that you wish to download. 
+#'@param destinations String vector list of destinations for requested files. Must be same length as \code{names}
+#'@param session Session object from \code{\link{authenticate_sb}}
 #'
-#'
+#'@author Luke Winslow
 #'
 #'@export
 item_file_download = function(id, names, destinations, session=NULL){
