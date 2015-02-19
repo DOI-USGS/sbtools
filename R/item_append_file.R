8e258beb5a9af2cc8e228b6d11e2b7a95c246abd
David Blodgett <dblodgett@usgs.gov>
2015-02-18 20:12:41 -0600

    an ignore addition for the stupid .DS_Store on mac.

--- R/item_append_file.R
+++ R/item_append_file.R
@@ -20,7 +20,11 @@ item_append_file = function(id, filename, session){
 		stop('Session state is invalid, please re-authenticate')
 	}
 	
-	POST(paste0(pkg.env$url_upload,'?id=', id), accept_json(), 
+  url<-paste0(pkg.env$url_upload,'?id=', id)
+  
+	POST(url, accept_json(), 
 			 body=list(file=upload_file(filename)), handle=session)
+  
+  return(item_get(id,session))
 	
 }
