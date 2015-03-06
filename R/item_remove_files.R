6b2f20453cf3ac5e4ea56d01345df3dff6d5fb9c
Jordan S Read <jread@usgs.gov>
2015-03-06 09:42:42 -0600

    new functions for item files

---
+++ R/item_remove_files.R
@@ -0,0 +1,8 @@
+#'@title Remove all files associated with an item
+#'@param item_id a sciencebase item identifier
+#'@param session Authenticated session object (from \link{authenticate_sb})
+#'@description Removes existing files associated with an item. 
+#'@export
+item_remove_files <- function(item_id, session){
+	item_update(item_id,list('files'=vector()), session = session)
+}
---
+++ R/item_replace_file.R
@@ -0,0 +1,13 @@
+#'@title Replace file associated with an item
+#'@param item_id a sciencebase item identifier
+#'@param files a character vector of file paths
+#'@param session Authenticated session object (from \link{authenticate_sb})
+#'@description replaces existing files associated with an item with a new one. 
+#'(Currently does not support multi-file uploads.) 
+#'This function will not append an existing collection of files. If that 
+#'is desired, use \code{\link{item_append_file}}
+#'@export
+item_replace_file <- function(item_id, file, session){	
+	item_remove_files(item_id, session)
+	item_append_file(id = item_id, filename = files, session)
+}
