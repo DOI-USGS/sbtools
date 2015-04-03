0de15e0fd96adc594e73c1d31c914b471dbfbbf5
Jordan S Read <jread@usgs.gov>
2015-04-03 18:47:30 -0500

    addresses #48

--- R/AAA.R
+++ R/AAA.R
@@ -1,6 +1,9 @@
 
 pkg.env <- new.env()
 
+.onAttach <- function(libname, pkgname) {
+	packageStartupMessage("This information is preliminary or provisional and is subject to revision. It is being provided to meet the need for timely best science. The information has not received final approval by the U.S. Geological Survey (USGS) and is provided on the condition that neither the USGS nor the U.S. Government shall be held liable for any damages resulting from the authorized or unauthorized use of the information.")
+}
 .onLoad = function(libname, pkgname){
 	set_endpoint()
 }
