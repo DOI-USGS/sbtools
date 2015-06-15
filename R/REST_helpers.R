0b8683d374d4b8e74078a8c12edaa94da2c081d5
Jordan S Read <jread@usgs.gov>
2015-06-15 09:12:38 -0500

    using XML in package

--- R/REST_helpers.R
+++ R/REST_helpers.R
@@ -1,6 +1,14 @@
-
+#' generic POSTs for sbtools package
+#' package wrapped for generic POSTs that test sessions internally and wrap some errors
+#' 
+#' @param url a base url for the POST
+#' @param body a POST body
+#' @param session a sbtools session object
+#' @param timeout a timeout for the request in seconds
+#' @param ... additional params passed to \code{\link[httr]{GET}}
 #' @export
 #' @keywords internal
+#' @import XML
 sbtools_POST <- function(url, body, session, timeout = default_timeout(), ...){
 	
 	if (!session_authorized(session))
@@ -16,14 +24,23 @@ sbtools_POST <- function(url, body, session, timeout = default_timeout(), ...){
 	return(r)
 }
 
+#' generic GETs for sbtools package
+#' package wrapped for generic GETs that test sessions internally and wrap some errors
+#' 
+#' @param url a base url for the GET
+#' @param query a list of query params 
+#' @param session a sbtools session object
+#' @param timeout a timeout for the request in seconds
+#' @param ... additional params passed to \code{\link[httr]{GET}}
 #' @export
 #' @keywords internal
+#' @import XML
 sbtools_GET <- function(url, query, session, timeout = default_timeout(), ...){
 	
 	if (!session_validate(session))
 		stop('session is not valid. See ?authenticate_sb')
 	
-	r = GET(url = url, query = query, handle=session, ...)
+	r = GET(url = url, query = query, handle=session, timeout(timeout), ...)
 	
 	if('errors' %in% names(content(r))){
 		stop(content(r)$errors$message)
