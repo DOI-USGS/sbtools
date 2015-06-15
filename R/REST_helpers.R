e1190fa42c5e38d5a8d84258ab1754b76ac6f288
Alison Appling <aappling@usgs.gov>
2015-06-15 15:28:18 -0500

    change order & naming of sbtools_GET args

--- R/REST_helpers.R
+++ R/REST_helpers.R
@@ -24,23 +24,23 @@ sbtools_POST <- function(url, body, session, timeout = default_timeout(), ...){
 	return(r)
 }
 
-#' generic GETs for sbtools package
-#' package wrapped for generic GETs that test sessions internally and wrap some errors
+#' generic GETs for sbtools package package wrapped for generic GETs that test
+#' sessions internally and wrap some errors
 #' 
 #' @param url a base url for the GET
-#' @param query a list of query params 
+#' @param ... additional params passed to \code{\link[httr]{GET}}, often
+#'   including \code{query}
 #' @param session a sbtools session object
 #' @param timeout a timeout for the request in seconds
-#' @param ... additional params passed to \code{\link[httr]{GET}}
 #' @export
 #' @keywords internal
 #' @import XML
-sbtools_GET <- function(url, query, session, timeout = default_timeout(), ...){
+sbtools_GET <- function(url, ..., session, timeout = default_timeout()){
 	
 	if (!session_validate(session))
 		stop('session is not valid. See ?authenticate_sb')
 	
-	r = GET(url = url, query = query, handle=session, timeout(timeout), ...)
+	r = GET(url = url, handle=session, timeout(timeout), ...)
 	
 	if('errors' %in% names(content(r))){
 		stop(content(r)$errors$message)
