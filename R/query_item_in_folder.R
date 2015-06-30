ea8c179646422f6e4851c0a55b839cd45ca7f3e8
Alison Appling <aappling@usgs.gov>
2015-06-30 12:28:18 -0500

    update doc for query_item_in_folder

--- R/query_item_in_folder.R
+++ R/query_item_in_folder.R
@@ -1,11 +1,12 @@
 #' Search within an SB folder
 #' 
-#' Search for text in the title, abstract, etc. within an SB folder
+#' Search for text in the title, abstract, etc. within an SB folder and any
+#' subfolders.
 #' 
 #' @param text text in the title, abstract, etc. of the desired item
 #' @param folder an SB item ID for the folder to search in
 #' @param ... Additional parameters are passed on to \code{\link[httr]{GET}}
-#' @param session (optional) SB Session to use, not provided queries public
+#' @param session (optional) SB Session to use, not provided queries public 
 #'   items only
 #' @param limit Max number of matching items to return
 #' @import httr
