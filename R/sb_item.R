ec34b0a8fbd2fe53949e8ab6cd386ae73b3653a0
Scott Chamberlain <myrmecocystus@gmail.com>
2015-09-18 17:11:48 -0700

    fixes to print.sbitem() method according to #105

--- R/sb_item.R
+++ R/sb_item.R
@@ -10,7 +10,7 @@
 #' library("httr")
 #' res <- query_items(list(s = "Search", q = "water", format = "json"))
 #' ids <- vapply(httr::content(res)$items, "[[", "", "id")
-#' lapply(ids[1:3], item_get)
+#' (out <- lapply(ids[1:3], item_get))
 #' 
 #' # create item class from only an item ID
 #' as.sbitem("4f4e4b24e4b07f02db6aea14")
@@ -19,29 +19,39 @@
 #' (x <- as.sbitem("4f4e4b24e4b07f02db6aea14"))
 #' as.sbitem(x)
 #' }
-sb_item <- function(x) {
-	structure(x, class = "sbitem")
-}
 
 #' @export
 #' @rdname sbitem
 as.sbitem <- function(x, ...) UseMethod("as.sbitem")
-#' @export
-as.sbitem.list <- function(x, ...) sb_item(x)
+
 #' @export
 as.sbitem.character <- function(x, ...) item_get(x, ...)
+
 #' @export
 as.sbitem.sbitem <- function(x, ...) x
+
+as.sbitem.list <- function(x, ...) sb_item(x)
 
 #' @export
 print.sbitem <- function(x, ...) {
 	cat("<ScienceBase Item>", "\n")
-	cat("  Item ID:     ", x$id, "\n", sep = "")
-	cat("  Parent ID:   ", x$parentId, "\n", sep = "")
-	cat("  Title:       ", x$title, "\n", sep = "")
+	cat("  Title: ", x$title, "\n", sep = "")
+	cat("  Creator/LastUpdatedBy:     ", x$provenance$createdBy, " / ", x$provenance$lastUpdatedBy, "\n", sep = "")
 	cat("  Provenance (Created / Updated):  ", x$provenance$dateCreated, 
 			" / ", x$provenance$lastUpdated, "\n", sep = "")
-	cat("  Children:    ", x$hasChildren, "\n", sep = "")
+	cat("  Children: ", x$hasChildren, "\n", sep = "")
+	cat("  Item ID: ", x$id, "\n", sep = "")
+	cat("  Parent ID: ", x$parentId, "\n", sep = "")
+}
+
+#' @export
+#' @rdname sbitem
+is.sbitem <- function(x) {
+	if (is(x, "sbitem")) TRUE else FALSE
+}
+
+sb_item <- function(x) {
+	structure(x, class = "sbitem")
 }
 
 pluck <- function(x, name, type) {
