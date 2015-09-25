ddf8253602a1a1fcaf13d47e13445df54daf6b97
Alison Appling <aappling@usgs.gov>
2015-09-25 15:06:55 -0500

    resolve #91

--- R/item_rm.R
+++ R/item_rm.R
@@ -3,6 +3,9 @@
 #' @param id item ID
 #' @param ... Additional parameters are passed on to \code{\link[httr]{GET}} and
 #'   \code{\link[httr]{DELETE}}
+#' @param recursive logical, FALSE by default. CAUTION: setting recursive=TRUE
+#'   means that not only will this item be deleted, but so will all its child
+#'   items and their child items and so on.
 #' @param session Session object from \code{\link{authenticate_sb}}
 #'   
 #' @return TRUE to indicate success
@@ -10,12 +13,14 @@
 #' @import httr
 #'   
 #' @export
-item_rm = function(id, ..., session=current_session()){
+item_rm = function(id, ..., recursive=FALSE, session=current_session()){
+	
+	if(isTRUE(recursive)) return(item_rm_recursive(id, ..., session=session))
 	
 	children = item_list_children(id, ..., limit=2, session=session)
 	
 	if(nrow(children) > 0){
-		stop('Item has children. Remove all children before deleting.')
+		stop('Item has children. To remove children, grandchildren, etc. set recursive=TRUE.')
 	}
 	
 	r = sbtools_DELETE(paste0(pkg.env$url_item, id, '?format=json'), ..., accept_json(), session=session)
--- R/item_rm_recursive.R
+++ R/item_rm_recursive.R
@@ -1,17 +1,19 @@
-#' Remove an item completely by recursively removing its children
-#'   
+#' Remove an item completely by recursively removing its child items
+#' 
 #' @export
 #' @param id (character) item ID
 #' @param ... Additional parameters are passed on to \code{\link[httr]{GET}} and
 #'   \code{\link[httr]{DELETE}}
 #' @param session Session object from \code{\link{authenticate_sb}}
 #' @return \code{TRUE} to indicate success
-#' @details BEWARE: This removes all folders/files in an item completely.
+#' @details BEWARE: This completely removes ALL CHILD ITEMS AND THEIR CHILDREN
+#'   as well as the item itself.
 #' @examples \dontrun{
 #' # Create an item with nested structure
+#' authenticate_sb()
 #' fname <- "chairs"
-#' folder_create(name = fname)
-#' df <- item_list_children(user_id())
+#' folder_create(user_id(), name = fname)
+#' df <- item_list_children(user_id()) # may need to wait a moment to run this
 #' id <- df[ df$title == fname, "id" ]
 #' folder_create(id, name = "one")
 #' folder_create(id, name = "two")
@@ -22,41 +24,45 @@
 #' item_rm_recursive(id)
 #' }
 item_rm_recursive = function(id, ..., session = current_session()) {
-	ogid <- id
-	while (length(id) > 0) {
-		whileid <- id
-		k <- lapply(id, item_list_children_all, session = session)
-		todel <- unlist(lapply(k, function(z) pluck(Filter(function(x) !x$hasChildren, z), "id", "")))
-		invisible(lapply(todel, file_delete, session = session))
-		id <- unlist(lapply(k, function(z) pluck(Filter(function(x) x$hasChildren, z), "id", "")))
-		any_left <- lapply(whileid, item_list_children_all, session = session)
-		if (length(id) == 0 && is.null(unlist(any_left))) {
-			if (whileid == ogid) {
-				id <- character(0)
-			} else {
-				id <- ogid
-			}
-		}
+	
+	# check args
+	if(length(id) != 1) stop('expecting exactly 1 id')
+	
+	# get list of children. no need to identify or delete files; these are deleted
+	# along with their containing items automatically
+	kids <- item_list_children_all(id, session = session)
+	kids <- lapply(kids, function(kid) as.data.frame(kid[c("id","hasChildren")], stringsAsFactors=FALSE))
+	kids <- do.call(rbind, kids)
+	
+	if(nrow(kids) > 0) {
+		# recursive case: has children. delete the children first
+		
+		# delete the children with children recursively. eventually delete the 
+		# children without children all at once; for now we'll just separate them into
+		# a second lapply loop
+		lapply(kids[kids$hasChildren, 'id'], item_rm_recursive, ..., session=session)
+		lapply(kids[!kids$hasChildren, 'id'], item_rm, ..., session=session)
+		
 		# maybe FIXME - there's a lag time between the requests above to delete things
-		# and when it actually happens - sleep 1 second before starting next 
-		# loop iteration
-		Sys.sleep(1)
+		# and when it actually happens - sleep 2 seconds before moving up one level in
+		# the recursion
+		Sys.sleep(2)
 	}
-	# maybe FIXME - same as above note
-	Sys.sleep(2)
-	# Finally, delete the top level item
-	invisible(item_rm(ogid, ..., session = session))
+	# base case: has no children. just delete.
+	item_rm(id, ..., session=session)	
+	
+	# return TRUE if we made it this so far
 	return(TRUE)
 }
 
-file_delete <- function(x, ..., session = session) {
-	res <- DELETE(paste0(pkg.env$url_item, x),
-								query = list(format = "json"),
-								content_type_json(),
-								handle = session, ...)
-	if (res$status_code == 200) TRUE
-}
-
+#' Like item_list_children except that it returns all fields, not just 'id' and
+#' 'title'
+#' 
+#' @param id SB item ID
+#' @param ... Additional parameters are passed on to \code{\link[httr]{GET}}
+#' @param session (optional) SB session from \link{authenticate_sb}
+#' @param limit Max children returned
+#' @keywords internal
 item_list_children_all <- function(id, ..., session = current_session(), limit = 100) {
 	query <- list(parentId = id, max = limit, format = 'json')
 	r <- sbtools_GET(url = pkg.env$url_items, ..., query = query, session = session)
