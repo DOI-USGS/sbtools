5b6f1a32f874be4adf24db34f7a2ed50c387b926
Alison Appling <aappling@usgs.gov>
2015-11-10 11:17:40 -0600

    fix REST tests

--- tests/testthat/test-REST.R
+++ tests/testthat/test-REST.R
@@ -3,7 +3,7 @@ Sys.sleep(2)
 test_that("generic post fails w/o auth", {
 	# auth fails locally:
 	expect_error(item_append_files("54e265a4e4b08de9379b4dfb", '/foo/bar/baz.zip'),'Item not found for')
-	session <- httr::handle("http://google.com", cookies = FALSE)
+	session <- httr::handle("http://google.com")
 	attributes(session) <- c(attributes(session), list(birthdate=Sys.time()))
 	
 	# auth passes locally, but POST fails due to files not existing:
@@ -17,22 +17,21 @@ test_that("generic post fails w/o auth", {
 
 context("sbtools_GET")
 public_item <- '4f4e4a62e4b07f02db636b68' # public read access
-private_item <- '55569325e4b0a92fa7e9cf36'
+non_item <-     '4f4e4a62e4b0a92fa7e9cf36' # made-up ID
+#private_item <- '55569325e4b0a92fa7e9cf36' # private to whom? can we test with a session that has access and yet is Travis-runable?
 
 test_that("generic get w/ and w/o auth", {
-	# expect_is(item_get(public_item, session = NULL), 'sbitem')
-	# expect_error(item_get(private_item, session = NULL), 'Item not found')
-	expect_error(item_get(public_item, session = NULL), 
-							 'session is not authorized')
-	
-	session <- httr::handle("http://google.com", cookies = FALSE)
+
+	session <- httr::handle("http://google.com") # cookies arg is ignored by httr so don't pass it
 	attributes(session) <- c(attributes(session), list(birthdate=Sys.time()))
-	expect_is(item_get(public_item, session = session), 'sbitem') # public get with 'valid' session
+
+	# public access to public items, with or without login
+	expect_is(item_get(public_item, session = NULL), 'sbitem')
+	expect_is(item_get(public_item, session = session), 'sbitem')
 	
-	expect_error(item_get(private_item, session = session))
-	set_expiration(as.difftime("00:00:01"))
-	Sys.sleep(2)
-	expect_error(item_get(public_item, session = session), 'session is not authorized')
+	# 'not found' error for missing items, with or without login
+	expect_error(item_get(non_item, session = NULL), 'Item not found')
+	expect_error(item_get(non_item, session = session), 'Item not found')
 	
 })
 
