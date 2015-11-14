45642150acc43728c5caddb1cd93f2f94d381e97
Scott Chamberlain <myrmecocystus@gmail.com>
2015-11-13 17:19:31 -0800

    added item_move function, fix #138

---
+++ R/item_move.R
@@ -0,0 +1,32 @@
+#' Move item from one folder to another
+#'
+#' @export
+#' @param id A ScienceBase ID or something that can be coerced to a SB item ID
+#' by \code{\link{as.sbitem}} 
+#' @param id_new Folder/item to move \code{id} to. A ScienceBase ID or something 
+#' that can be coerced to a SB item ID by \code{\link{as.sbitem}} 
+#' @param ... Additional parameters are passed on to \code{\link[httr]{PUT}}
+#' @param session Authenticated session object (from \link{authenticate_sb})
+#'
+#' @return An object of class \code{sbitem}. Same as \code{id}, but with new 
+#' parent id
+#' 
+#' @examples \dontrun{
+#' # create 1st folder
+#' (fold1 <- folder_create(user_id(), "bear123"))
+#' (res <- item_create(fold1, "item-to-move"))
+#' 
+#' # create 2nd folder
+#' (fold2 <- folder_create(user_id(), "bear456"))
+#' 
+#' # move item in 1st folder to 2nd folder
+#' (res2 <- item_move(res, fold2))
+#' 
+#' # test identical
+#' identical(res2$parentId, fold2$id)
+#' }
+item_move <- function(id, id_new, ..., session = current_session()) {
+	id <- as.sbitem(id)
+	id_new <- as.sbitem(id_new)
+	item_update(id, list(parentId = id_new$id), ...)
+}
