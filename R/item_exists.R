1288a768d57cd2f20c8c53b7ee6e0766be485ece
Scott Chamberlain <myrmecocystus@gmail.com>
2015-11-25 15:06:21 -0800

    fixed item_get and item_list_files to accept sbitem objects and char strings fix #130 also added two eg data sets in inst/examples to make egs easier for file related stuff a bit of cleanup added convenience fxn for doing sesssion auth check session_val(), not deployed elsewhere yet added recursive param to item_list_files to recursively list files in children

--- R/item_exists.R
+++ R/item_exists.R
@@ -1,14 +1,14 @@
 #' check if identifier touple already exists on SB
-#' 
+#'
 #' returns TRUE if touple already belongs to a sciencebase item, FALSE if not
-#' 
+#'
 #' @param scheme the identifier scheme
 #' @param type the identifier type
 #' @param key the identifier key
 #' @param ... Additional parameters are passed on to \code{\link[httr]{GET}}
 #' @param session an SB session
 #' @return boolean for whether item exists
-#' 
+#'
 #' @examples
 #' \dontrun{
 #' item_exists('mda_streams','ts_doobs','nwis_01018035')
@@ -16,11 +16,11 @@
 #' }
 #' @export
 item_exists = function(scheme, type, key, ..., session=current_session()){
-	
+
 	if(!session_validate(session)){
 		stop('Session state is invalid, please re-authenticate')
 	}
-	
+
 	items <- query_item_identifier(scheme=scheme, ..., type=type, key=key, session=session)
 	if (nrow(items) > 0){
 		return(TRUE)
--- R/item_list_files.R
+++ R/item_list_files.R
@@ -1,6 +1,7 @@
 #' @title Get list of files attached to SB item
 #'
 #' @param id item ID
+#' @param recursive (logical) List files recursively. Default: \code{FALSE}
 #' @param session Session object from \link{authenticate_sb}
 #' @param ... Additional parameters are passed on to \link[httr]{GET}
 #'
@@ -11,23 +12,66 @@
 #' @export
 #' @examples \dontrun{
 #' item_list_files("4f4e4b24e4b07f02db6aea14")
+#' 
+#' # list files recursively
+#' ## create item
+#' id <- item_create(user_id(), title="some title")
+#' ## 1. create nested item w/ file
+#' file <- system.file("examples", "books.json", package = "sbtools")
+#' id2 <- item_create(id, title = "newest-thing")
+#' item_upload_create(id2, file)
+#' ## 2. create nested item w/ file
+#' file <- system.file("examples", "species.json", package = "sbtools")
+#' id3 <- item_create(id, title = "a-new-thing")
+#' item_upload_create(id3, file)
+#' ## 3. create nested item w/ file
+#' file <- system.file("examples", "data.csv", package = "sbtools")
+#' id4 <- item_create(id, title = "another-thing")
+#' item_upload_create(id4, file)
+#' item_list_files(id = '56562348e4b071e7ea53e09d', recursive = FALSE) # default
+#' item_list_files(id = '56562348e4b071e7ea53e09d', recursive = TRUE)
 #' }
-item_list_files = function(id, ..., session=current_session()){
+item_list_files = function(id, recursive = FALSE, ..., session=current_session()){
 	
-	if(!session_validate(session)){
-		stop('Session state is invalid, please re-authenticate')
+	session_val(session)
+	
+	id <- as.sbitem(id)
+	item = item_get(id, session = session)
+	
+	if (recursive) {
+		if (item$hasChildren) {
+			kids <- item$hasChildren
+			out <- list()
+			i <- 0
+			while (kids) {
+				i <- i + 1
+				if (!is(item, "list")) item <- list(item)
+				item <- lapply(item, function(w) {
+					if (is(w, "data.frame")) w$id else w
+				})
+				if (!is(item[[1]], "sbitem")) item <- unlist(item, FALSE)
+				item <- lapply(item, item_list_children, session = session)
+				children <- lapply(item, function(z) lapply(z$id, as.sbitem))
+				out[[i]] <- children
+				kids <- any(comp(sapply(unlist(children, FALSE), "[[", "hasChildren")))
+			}
+			out <- unlist(lapply(out, function(x) if (is(x, "list")) unlist(x, FALSE) else x), FALSE)
+			files <- unlist(comp(sapply(out, "[[", "files")), recursive = FALSE)
+		} else {
+			files <- list()
+			message("no child items found")
+		}
+	} else {
+		files <- item$files
 	}
 	
-	item = item_get(id, ..., session=session)
+	out <- data.frame(stringsAsFactors = FALSE)
 	
-	files = item$files
-	out = data.frame()
-	
-	if(length(files)==0){
+	if (length(files) == 0) {
 		return(out)
 	}
 	
-	for(i in 1:length(files)){
+	for (i in 1:length(files)) {
 		out[i,'fname'] = files[[i]]$name
 		out[i,'size'] = files[[i]]$size
 		out[i,'url'] = files[[i]]$url
