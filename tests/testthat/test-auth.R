c235a154042fe715e609a0d60a22bfa6913fbc14
Luke Winslow <lwinslow@usgs.gov>
2015-12-30 15:52:31 -0600

    added tests that require authentication

---
+++ tests/testthat/test-auth.R
@@ -0,0 +1,54 @@
+
+
+
+context("test sb functionality requiring authentication")
+
+test_that("authenticate_sb login results in valid session", {
+	
+	if(is.na(Sys.getenv("sb_user", unset=NA))){
+		skip("Authenticated tests skipped due to lack of login info")
+	}
+	
+	
+	expect_silent(authenticate_sb(Sys.getenv("sb_user", unset=""), Sys.getenv("sb_pass", unset="")))
+	expect_true(session_validate())
+	
+	expect_equal(nchar(user_id()), 24)
+	
+})
+
+test_that("item creation, identifiers, and file upload works", {
+	
+	if(is.na(Sys.getenv("sb_user", unset=NA))){
+		skip("Authenticated tests skipped due to lack of login info")
+	}
+	
+	#create an item
+	item = item_create(title="automated testing item")
+	expect_is(item, 'sbitem')
+	
+	#add item identifier
+	rand_key = as.character(floor(runif(1, 0, 10^12)))
+	expect_is(item_update_identifier(item, 'test_scheme', 'test_type', rand_key), 'sbitem')
+	
+	#upload file
+	expect_is(item_append_files(item, 'examples/data.csv'), 'sbitem')
+	
+	#download file
+	dir_name = tempdir()
+	dl_files = item_file_download(item, dest_dir=dir_name)
+	
+	expect_equal(file.info(dl_files)$size, file.info('examples/data.csv')$size)
+	
+	#check item identifier (checking issue #74)
+	ident = item_get(item)$identifier
+	
+	expect_equal(ident[[1]]$type, "test_type")
+	expect_equal(ident[[1]]$scheme, "test_scheme")
+	expect_equal(ident[[1]]$key, rand_key)
+	
+})
+
+
+
+
