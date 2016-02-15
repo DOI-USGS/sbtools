70e7bbc801c16e10f4ebf5fd520ef62472440013
Alison Appling <aappling@usgs.gov>
2016-02-15 10:35:11 -0600

    fix #182

--- R/session_authorized.R
+++ R/session_authorized.R
@@ -1,4 +1,10 @@
-session_authorized <- function(session){
+#' Determine whether a session is authenticated
+#' 
+#' Returns TRUE if the session is a valid, non-NULL, active SB session.
+#' 
+#' @param session The session to check
+#' @export
+session_authorized <- function(session=current_session()){
 	
 	return(session_validate(session) && !is.null(session))
 	
