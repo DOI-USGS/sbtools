c7a5ddb5b09daa33a6fab60989108913ff8d3525
Luke Winslow <lwinslow@usgs.gov>
2016-04-08 16:29:40 -0500

    changing default as per #141

--- R/item_replace_files.R
+++ R/item_replace_files.R
@@ -4,14 +4,14 @@
 #' @param all A boolean indicating if all attached files should be removed before
 #' uploading new files. FALSE if only files with matching names should be replaced. 
 #' If you wish to upload files with duplicate names, see \code{\link{item_append_files}}.
-#' Defaults to TRUE.
+#' Defaults to FALSE.
 #' 
 #' @description replaces existing files associated with an item with a new one. 
 #'   (Currently does not support multi-file uploads.) This function will not
 #'   append an existing collection of files. If that is desired, use
 #'   \code{\link{item_append_files}}
 #' @export
-item_replace_files <- function(sb_id, files, ..., all=TRUE, session=current_session()){
+item_replace_files <- function(sb_id, files, ..., all=FALSE, session=current_session()){
 	if(all){
 		item_rm_files(sb_id, ..., session=session)
 	}else{
