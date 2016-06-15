d9162eafbbb44344ef43ea69e92f53c6b27c3741
Luke Winslow <lwinslow@usgs.gov>
2016-06-15 15:24:50 -0500

    fix non-USGS DOI for reviewer

--- R/query_sb_doi.R
+++ R/query_sb_doi.R
@@ -16,7 +16,7 @@
 #' #Two example DOI-specific queries
 #' query_sb_doi('10.5066/F7M043G7')
 #' 
-#' query_sb_doi('10.1126/science.aab1345')
+#' query_sb_doi('10.5066/F7Z60M35')
 #' 
 #' @export
 query_sb_doi = function(doi, ..., limit=20, session=current_session()){
