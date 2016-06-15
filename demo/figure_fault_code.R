e1661d7d08327ba16082a4323ff0a2e51c8592bb
Luke Winslow <lwinslow@usgs.gov>
2016-06-15 17:44:44 -0500

    dialed in the figure size

--- demo/figure_fault_code.R
+++ demo/figure_fault_code.R
@@ -7,7 +7,7 @@ library(maps)
 
 faults = query_sb(list(q="faults", browseType = "OGC WFS Layer"), limit=20)
 
-png('faultlinefig.png', res=300, width=1700, height=1400)
+#png('faultlinefig.png', res=300, width=1700, height=1400)
 par(mar=c(5,5,1,1), oma=c(0,2,0,0))
 map('usa')
 for(i in 1:length(faults)){
@@ -21,4 +21,4 @@ for(i in 1:length(faults)){
 map.axes()
 mtext('lon', 1, line=2.2, mex=2)
 mtext('lat', 2, line=2.2, mex=2)
-dev.off()
+#dev.off()
