a2c3ce3c36d9d82a45e6c69c7f3e6c83d77605e4
David Blodgett <dblodgett@usgs.gov>
2023-11-20 08:12:40 -0600

    fix up item_get_fields

--- R/item_get_fields.R
+++ R/item_get_fields.R
@@ -9,19 +9,29 @@
 #' @export
 #' @examples \donttest{
 #' # Get certain fields from an item
-#' item_get_fields("4f4e4b24e4b07f02db6aea14", c('title', 'citation', 'contacts'))
+#' item_get_fields("63cb38b2d34e06fef14f40ad", c('title', 'citation', 'contacts'))
 #' 
 #' #' # If only 1 field selection, do or don't drop list format
-#' item_get_fields("4f4e4b24e4b07f02db6aea14", 'title')
-#' item_get_fields("4f4e4b24e4b07f02db6aea14", 'title', drop = FALSE)
+#' item_get_fields("63cb38b2d34e06fef14f40ad", 'title')
+#' item_get_fields("63cb38b2d34e06fef14f40ad", 'title', drop = FALSE)
 #' }
 item_get_fields = function(sb_id, fields, ..., drop=TRUE){
+	
 	sb_id = as.sbitem(sb_id)
+	
+	if(is.null(sb_id)) return(NULL)
+	
 	query <- list('fields'=paste0(fields, collapse=","), 'type'='json')
+	
 	r <- sbtools_GET(url=paste0(pkg.env$url_item, sb_id$id), ..., query = query)
+	
 	if(is(r, "list") && r$status == 404) {
 		return(NULL)
 	}
-	out <- content(r)
+	
+	out <- try(content(r))
+	
+	if(inherits(out, "try-error")) return(NULL)
+	
 	return(if(length(fields)==1 && drop) out[[fields]] else out[fields])
 }
