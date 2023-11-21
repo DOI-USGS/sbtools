146917a48d0a70ed29ecb3f6ffdc52e1435f1def
David Blodgett <dblodgett@usgs.gov>
2023-11-21 13:40:02 -0600

    combine item_get functions

--- R/item_get.R
+++ R/item_get.R
@@ -40,3 +40,66 @@ get_item <- function(id, ...) {
 	
 	return(as.sbitem(content(res)))
 }
+
+#' @title Get an item's parent ID
+#'
+#' @template manipulate_item
+#' @return An item object representing the parent of the supplied item.
+#' 
+#' @description 
+#' Retrieves the parent of a supplied item based on the ScienceBase
+#' item tree hierarchy. 
+#' 
+#' @export
+#' @examples \donttest{
+#' item_get_parent("4f4e4b24e4b07f02db6aea14")
+#' 
+#' item_get_parent(item_get("4f4e4b24e4b07f02db6aea14"))
+#' }
+item_get_parent = function(sb_id, ...) {
+	res <- as.sbitem(sb_id, ...)
+	
+	if(is.null(res)) return(NULL)
+	
+	return(as.sbitem(res$parentId))
+}
+
+#' Retrieve specific fields from an SB item
+#' 
+#' @template manipulate_item
+#' @param fields a vector of fields
+#' @param drop logical. If only one field is selected, should the
+#'   list format be dropped?
+#' @return List serialization of chosen metadata for an SB item
+#' @import httr
+#' @export
+#' @examples \donttest{
+#' # Get certain fields from an item
+#' item_get_fields("63cb38b2d34e06fef14f40ad", c('title', 'citation', 'contacts'))
+#' 
+#' #' # If only 1 field selection, do or don't drop list format
+#' item_get_fields("63cb38b2d34e06fef14f40ad", 'title')
+#' item_get_fields("63cb38b2d34e06fef14f40ad", 'title', drop = FALSE)
+#' }
+item_get_fields = function(sb_id, fields, ..., drop=TRUE){
+	
+	sb_id = as.sbitem(sb_id)
+	
+	if(is.null(sb_id)) return(NULL)
+	
+	query <- list('fields'=paste0(fields, collapse=","), 'type'='json')
+	
+	r <- sbtools_GET(url=paste0(pkg.env$url_item, sb_id$id), ..., query = query)
+	
+	if(is(r, "list") && r$status == 404) {
+		return(NULL)
+	}
+	
+	out <- try(content(r))
+	
+	if(inherits(out, "try-error")) return(NULL)
+	
+	return(if(length(fields)==1 && drop) out[[fields]] else out[fields])
+}
+
+
